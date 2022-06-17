d6e77295c66be6b4dc6fb5cdb851e7ffa6d8de9e
andribob26 <andri.feb.26@gmail.com>
2022-06-17 16:18:13 +0700

    Updates

--- src/component/Footer.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const Footer = () => {
-  return (
-    <div className="py-3 text-xs text-zinc-600"><p>&copy;2022 - Built by Andri febrian using ReactJs & TailwindCss</p></div>
-  )
-}
-
-export default Footer
